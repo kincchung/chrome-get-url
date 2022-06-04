17de3c09604cc39430fc4a7793cdf0c1ca532bab
kincchung <kincchung@gmail.com>
2022-06-05 02:41:28 +0800

    first commit

--- background/bg.js
+++ background/bg.js
@@ -1,17 +1,12 @@
-var sus = ['.xyz/'];
+chrome.tabs.onActivated.addListener( function(activeInfo){
+    chrome.tabs.get(activeInfo.tabId, function(tab){
+        y = tab.url;
+        console.log("you are here: "+y);
+    });
+});
 
-var searchEngine = ['google.com'];
-
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        if('initiator' in details === true){
-            if(details.initiator.includes(searchEngine)){}
-            else if(details.url.includes(sus) && (details.url.includes(details.initiator) === false)) {
-                console.log(details)
-                return { redirectUrl: details.initiator }
-            }
-        }
-    },
-    { urls: ['https://*/*', 'http://*/'] },
-    ['blocking']
-)
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+    if (tab.active && change.url) {
+        console.log("you are here: "+change.url);           
+    }
+});
